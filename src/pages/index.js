f46942fc674ecd1e355056816e168fb48e26c721
Rodolfo Palacios <rpalacios@igloosoftware.com>
2018-07-21 12:06:21 -0400

    Alignment issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const GridContainer = styled.div`
   grid-auto-rows: 300px;
   grid-column-gap: 24px;
   row-gap: 24px;
-  margin: 64px 32px;
 
   @media (max-width: 700px) {
     grid-template-columns: 1fr [col-start];
